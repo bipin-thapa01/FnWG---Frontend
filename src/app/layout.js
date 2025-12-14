e53389d9dd7d7a7161b17279eecb8587cf7ba398
Bipin Thapa <yukichin638@gmail.com>
2025-12-14 21:14:22 +0545

    initial commit and nav bar initial design

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Nav from "./nav/nav";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Nav/>
         {children}
       </body>
     </html>
---
+++ src/app/nav/nav.js
@@ -0,0 +1,49 @@
+'use client'
+
+import React from 'react';
+import {useState, useEffect, useRef } from 'react';
+import { usePathname } from 'next/navigation';
+import { IoApps } from "react-icons/io5";
+import { FaBell } from "react-icons/fa";
+import { SlGraph } from "react-icons/sl";
+import { GrPlan } from "react-icons/gr";
+import { CiSaveDown1 } from "react-icons/ci";
+import { IoFastFood } from "react-icons/io5";
+
+export default function Nav() {
+
+  const pathname = usePathname();
+
+  return (
+    <div className='fixed h-full top-0 flex flex-col m-1 border-r w-64 px-4' style={{ borderColor: '#1d2939' }}>
+      <div className='py-4 mb-4'>Logo</div>
+      <div>
+        <div className='text-gray-500 mb-4 text-sm'>MENU</div>
+        <div className='flex flex-col gap-x-1.5'>
+          <div className={`h-10 pl-1 cursor-pointer hover:bg-gray-800 rounded-lg text-center flex items-center gap-x-4 ${pathname == '/' ? 'bg-gray-800 text-blue-600' : ''}`}><IoApps />
+            <span>Dashboard</span>
+          </div>
+          <div className={`h-10 pl-1 cursor-pointer hover:bg-gray-800 rounded-lg text-center flex items-center gap-x-4 ${pathname == '/remainder' ? 'bg-gray-800 text-blue-600' : ''}`}>
+            <FaBell />
+            <span>Remainder</span>
+          </div>
+          <div className={`h-10 pl-1 cursor-pointer hover:bg-gray-800 rounded-lg text-center flex items-center gap-x-4 ${pathname == '/streak' ? 'bg-gray-800 text-blue-600' : ''}`}>
+          <SlGraph />
+          <span>Streak</span>
+          </div>
+          <div className={`h-10 pl-1 cursor-pointer hover:bg-gray-800 rounded-lg text-center flex items-center gap-x-4 ${pathname == '/workout-planning' ? 'bg-gray-800 text-blue-600' : ''}`}>
+          <GrPlan />
+          <span>Workout Planning</span>
+          </div>
+          <div className={`h-10 pl-1 cursor-pointer hover:bg-gray-800 rounded-lg text-center flex items-center gap-x-4 ${pathname == '/calorie-tracker' ? 'bg-gray-800 text-blue-600' : ''}`}>
+          <CiSaveDown1 />
+          <span>Calorie Tracker</span>
+          </div>
+          <div className={`h-10 pl-1 cursor-pointer hover:bg-gray-800 rounded-lg text-center flex items-center gap-x-4 ${pathname == '/food-nutrition' ? 'bg-gray-800 text-blue-600' : ''}`}>
+          <IoFastFood />
+          <span>Food Nutrition</span></div>
+        </div>
+      </div>
+    </div>
+  )
+}
